69699e61a61ff078bdd48eaf4121912fa3153f72
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-24 02:59:32 +0530

    My 1st commit for jenkins-demo

--- user_data_master.sh
+++ user_data_master.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-# Add required dependencies for the jenkins package
 sudo yum install java-21-amazon-corretto -y
+sudo yum install git -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo yum install chkconfig -y
 sudo chkconfig jenkins on
-sudo yum install git -y
